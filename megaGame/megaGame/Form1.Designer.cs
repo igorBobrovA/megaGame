9bb3476c140d2f4e03ed0e32a1971ed28ba0d792
igorBobrovA <57355113+igorBobrovA@users.noreply.github.com>
2019-12-04 18:55:46 +0700

    alfo_Game0.0.1_cubs_alfo_menu0.2

--- megaGame/megaGame/Form1.Designer.cs
+++ megaGame/megaGame/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
@@ -40,23 +41,29 @@
             this.label9 = new System.Windows.Forms.Label();
             this.label10 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label13 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
-            this.button1.Location = new System.Drawing.Point(15, 15);
+            this.button1.Location = new System.Drawing.Point(16, 15);
             this.button1.Margin = new System.Windows.Forms.Padding(6);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(439, 51);
+            this.button1.Size = new System.Drawing.Size(440, 52);
             this.button1.TabIndex = 0;
             this.button1.Text = "НАЧАТЬ ИГРУ";
             this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(463, 28);
+            this.label1.Location = new System.Drawing.Point(464, 29);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(29, 25);
             this.label1.TabIndex = 1;
@@ -65,7 +72,8 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(486, 28);
+            this.label2.Location = new System.Drawing.Point(486, 29);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(24, 25);
             this.label2.TabIndex = 2;
@@ -74,7 +82,8 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(507, 28);
+            this.label3.Location = new System.Drawing.Point(508, 29);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(24, 25);
             this.label3.TabIndex = 3;
@@ -83,7 +92,8 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(527, 28);
+            this.label4.Location = new System.Drawing.Point(528, 29);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(24, 25);
             this.label4.TabIndex = 4;
@@ -92,7 +102,8 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(630, 28);
+            this.label5.Location = new System.Drawing.Point(630, 29);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(24, 25);
             this.label5.TabIndex = 8;
@@ -101,7 +112,8 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(608, 28);
+            this.label6.Location = new System.Drawing.Point(608, 29);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(29, 25);
             this.label6.TabIndex = 7;
@@ -110,7 +122,8 @@
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(587, 28);
+            this.label7.Location = new System.Drawing.Point(588, 29);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(24, 25);
             this.label7.TabIndex = 6;
@@ -119,7 +132,8 @@
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(569, 28);
+            this.label8.Location = new System.Drawing.Point(568, 29);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(24, 25);
             this.label8.TabIndex = 5;
@@ -127,10 +141,12 @@
             // 
             // label9
             // 
+            this.label9.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.label9.AutoSize = true;
             this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label9.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
-            this.label9.Location = new System.Drawing.Point(125, 72);
+            this.label9.Location = new System.Drawing.Point(124, 71);
+            this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(529, 37);
             this.label9.TabIndex = 9;
@@ -139,7 +155,8 @@
             // label10
             // 
             this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(660, 9);
+            this.label10.Location = new System.Drawing.Point(660, 10);
+            this.label10.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(0, 25);
             this.label10.TabIndex = 10;
@@ -147,19 +164,55 @@
             // button2
             // 
             this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
-            this.button2.Location = new System.Drawing.Point(12, 651);
+            this.button2.Location = new System.Drawing.Point(4, 652);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(1297, 53);
-            this.button2.TabIndex = 11;
+            this.button2.Size = new System.Drawing.Size(1296, 54);
+            this.button2.TabIndex = 13;
             this.button2.Text = "бросить кубик";
             this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label11.ForeColor = System.Drawing.Color.MediumSlateBlue;
+            this.label11.Location = new System.Drawing.Point(125, 108);
+            this.label11.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(83, 31);
+            this.label11.TabIndex = 14;
+            this.label11.Text = "игрок";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label12.ForeColor = System.Drawing.Color.MediumSlateBlue;
+            this.label12.Location = new System.Drawing.Point(500, 108);
+            this.label12.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(153, 31);
+            this.label12.TabIndex = 15;
+            this.label12.Text = "компьютер";
+            // 
+            // label13
+            // 
+            this.label13.Location = new System.Drawing.Point(292, 140);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(124, 124);
+            this.label13.TabIndex = 16;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
-            this.ClientSize = new System.Drawing.Size(1321, 716);
+            this.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.ClientSize = new System.Drawing.Size(1320, 715);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.label10);
             this.Controls.Add(this.label9);
@@ -176,6 +229,7 @@
             this.Margin = new System.Windows.Forms.Padding(6);
             this.Name = "Form1";
             this.Text = "megaGame";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -195,6 +249,10 @@
         private System.Windows.Forms.Label label9;
         private System.Windows.Forms.Label label10;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label13;
     }
 }
 
--- megaGame/megaGame/Form1.cs
+++ megaGame/megaGame/Form1.cs
@@ -12,10 +12,91 @@ namespace megaGame
 {
     public partial class Form1 : Form
     {
+        Random R;
         public Form1()
         {
             InitializeComponent();
         }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label11.Visible = false;
+            label12.Visible = false;
+            button2.Enabled = false;
+        }
 
+        public int NewGame(int player)
+        {
+            label9.Text = "Сёйчас ходит:";
+            label11.Visible = true;
+            label12.Visible = true;
+            if (player == 1)
+            {
+                label11.BackColor = Color.CornflowerBlue;
+                label12.BackColor = Color.PaleTurquoise;
+                button2.Enabled = true;
+                MessageBox.Show("сейчас вы ходите", "Кто ходит?", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else if (player == 2)
+            {
+                label12.BackColor = Color.CornflowerBlue;
+                label11.BackColor = Color.PaleTurquoise;
+                button2.Enabled = false;
+                MessageBox.Show("сейчас ходит компьютер", "Кто ходит?", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            return player;
+        }
+
+        public void cub(int number)
+        {
+                //Label label1 = new Label();
+                //Image image1 = Image.FromFile("C:.Users.1.Desktop.123.megaGame.megaGame.megaGame.cubs.cub1.png");
+                if (number == 1)
+                {
+
+                }
+                else if (number == 2)
+                {
+
+                }
+                else if (number == 3)
+                {
+
+                }
+                else if (number == 4)
+                {
+
+                }
+                else if (number == 5)
+                {
+
+                }
+                else if (number == 6)
+                {
+
+                }
+                //label1.Size = new Size(image1.Width, image1.Height);
+                //label1.Image = image1;
+        }
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            R = new Random();
+            int number = R.Next(1, 7),
+                WhoPlayNow = R.Next(1, 3),
+                compGo = 0,
+                playerGo = 0;
+            NewGame(WhoPlayNow);
+            if (WhoPlayNow == 2)
+            {
+                cub(number);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int number = R.Next(1, 7);
+            cub(number);
+        }
     }
 }
